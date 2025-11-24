abe8cbd9a8e458de4369c925b34761d92f404597
Gouveia888 <tiagogfilipe@gmail.com>
2025-11-24 22:56:54 +0000

    Correçao MainLayout

--- 3_Ano/1_Semestre/PWEB/Ficha4/GestaoLoja/obj/Debug/net8.0/GestaoLoja.AssemblyInfo.cs
+++ 3_Ano/1_Semestre/PWEB/Ficha4/GestaoLoja/obj/Debug/net8.0/GestaoLoja.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoLoja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e1ab26c0a84cb0c424643d24e6bfbcebd7c0822")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9710239f76ae314e653b5f7501d439fe5d927ade")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoLoja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoLoja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
